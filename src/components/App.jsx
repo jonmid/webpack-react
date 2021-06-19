7b5fcc75e03798578854c828b668d807bc9a0b98
JonMid <10jonmid@gmail.com>
2021-06-18 20:12:12 -0500

    [first] first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h1>Hello React desde Webpack!</h1>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    // filename: '[name].[contenthash].js',
+    // assetModuleFilename: 'assets/images/[hash][ext][query]',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+    },
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: [{ loader: 'html-loader' }],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      // {
+      //   test: /\.png/,
+      //   type: 'asset/resource',
+      // },
+      // {
+      //   test: /\.(woff|woff2)$/,
+      //   use: {
+      //     loader: 'url-loader',
+      //     options: {
+      //       limit: 10000,
+      //       mimetype: 'application/font-woff',
+      //       name: '[name].[contenthash].[ext]',
+      //       outputPath: './assets/fonts/',
+      //       publicPath: '../assets/fonts/',
+      //       esModule: false,
+      //     },
+      //   },
+      // },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, 'src', 'assets/images'),
+    //       to: 'assets/images',
+    //     },
+    //   ],
+    // }),
+    // new Dotenv(),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+};
